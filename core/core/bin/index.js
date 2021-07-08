3efc5f286a7dd43a8ae37defb5a2dab23773b9bb
chenguanghui <guanghui.c@shulidata.com>
2021-07-08 17:47:10 +0800

    chore:增加yargs

--- core/core/bin/index.js
+++ core/core/bin/index.js
@@ -1,13 +1,28 @@
 #!/use/bin/env node
 
-const importLocal = require('import-local')
+const importLocal = require("import-local");
+const colors = require("colors");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
-if(importLocal(__filename)){
-    require('npmlog').info('cli','正在使用cli本地版本')
-}else {
-    require('../lib')(process.argv.slice(2))
+if (importLocal(__filename)) {
+  require("npmlog").info("cli", "正在使用cli本地版本");
+} else {
+  require("../lib")(process.argv.slice(2));
 }
 
 //注册命令
-const argv = require('process').argv;
-console.log(argv,'注册命令');
+const argv = yargs(hideBin(process.argv))
+  .usage("Usage: $0 <command> [options]")
+//   .command({
+//     command: 'configure <key> [value]',
+//     aliases: ['config', 'cfg'],
+//     desc: 'Set a config variable',
+//     builder: (yargs) => yargs.default('value', 'true'),
+//     handler: (argv) => {
+//       console.log(`setting ${argv.key} to ${argv.value}`)
+//     }
+//   })
+  .help().argv;
+
+console.log(argv, "注册命令".red);
